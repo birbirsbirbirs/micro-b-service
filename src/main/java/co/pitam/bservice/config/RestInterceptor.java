26f0aac60748a92d40c19f32757df3e54f6023d3
birbirs\birbirsbirbirs <birbirsbirbirs@gmail.com>
2024-04-17 22:19:54 -0500

    otel updated

--- src/main/java/co/pitam/bservice/config/RestInterceptor.java
+++ src/main/java/co/pitam/bservice/config/RestInterceptor.java
@@ -1,23 +1,31 @@
 package co.pitam.bservice.config;
 
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
+import io.opentelemetry.context.Context;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Enumeration;
-
+@RequiredArgsConstructor
 @Slf4j
 @Configuration
 public class RestInterceptor extends OncePerRequestFilter {
+    private final Tracer tracer;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         log.info("doFilterInternal");
         Enumeration<String> headerNames = request.getHeaderNames();
+        Span span = tracer.currentSpan();
+        Context currentContext = Context.current();
         filterChain.doFilter(request, response);
     }
 }
