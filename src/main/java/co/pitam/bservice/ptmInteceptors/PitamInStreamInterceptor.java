2318caa7103e74ba9a6d86bbbad5f4b03339058b
birbirs\birbirsbirbirs <birbirsbirbirs@gmail.com>
2024-04-20 15:11:13 -0500

    rabbit mq configuration

---
+++ src/main/java/co/pitam/bservice/ptmInteceptors/PitamInStreamInterceptor.java
@@ -0,0 +1,20 @@
+package co.pitam.bservice.ptmInteceptors;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.config.GlobalChannelInterceptor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+
+@Slf4j
+@Configuration
+@GlobalChannelInterceptor(patterns = {"*in-*"})
+public class PitamInStreamInterceptor implements ChannelInterceptor {
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        MessageHeaderAccessor mutableAccessor = MessageHeaderAccessor.getMutableAccessor(message);
+        return ChannelInterceptor.super.preSend(message, channel);
+    }
+}
--- src/main/java/co/pitam/bservice/service/HeroListener.java
+++ src/main/java/co/pitam/bservice/service/HeroListener.java
@@ -2,6 +2,7 @@ package co.pitam.bservice.service;
 
 
 import co.pitam.bservice.model.Hero;
+import io.micrometer.tracing.Tracer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,8 +12,13 @@ import java.util.function.Consumer;
 @Slf4j
 @Configuration
 public class HeroListener {
+
     @Bean
-    public Consumer<Hero> inputsqs() {
-        return i -> log.info("input sqs: {}", i);
+    public Consumer<Hero> input(Tracer tracer) {
+        return i -> {
+            log.info("input sqs: {}", i);
+            log.info("<ACCEPTANCE_TEST> <TRACE:{}> Hello from consumer",
+                    tracer.currentSpan().context().traceId());
+        };
     }
 }
