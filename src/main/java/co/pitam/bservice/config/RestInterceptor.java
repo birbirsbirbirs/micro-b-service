72f45c93f0fde81294ba26c80f51e4bcdb88c0d4
birbirs\birbirsbirbirs <birbirsbirbirs@gmail.com>
2024-04-19 23:06:49 -0500

    to clear out the baggages

--- src/main/java/co/pitam/bservice/config/RestInterceptor.java
+++ src/main/java/co/pitam/bservice/config/RestInterceptor.java
@@ -7,24 +7,28 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.actuate.autoconfigure.tracing.TracingProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 @RequiredArgsConstructor
 @Slf4j
 @Configuration
 public class RestInterceptor extends OncePerRequestFilter {
     private final Tracer tracer;
+    private final TracingProperties tracingProperties;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if(!tracer.getAllBaggage().containsKey("power")){
-            tracer.createBaggageInScope("power", null);
-        }
-        if(!tracer.getAllBaggage().containsKey("customerId")){
-            tracer.createBaggageInScope("customerId", null);
-        }
+
+        Map<String, String> allBaggage = tracer.getAllBaggage();
+        List<String> fields = tracingProperties.getBaggage().getCorrelation().getFields();
+        fields.parallelStream().filter(p->!allBaggage.containsKey(p)).forEach(p->tracer.createBaggageInScope(p,null));
+
         filterChain.doFilter(request, response);
     }
 }
