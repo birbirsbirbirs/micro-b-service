30e6d92a49d25e21febca02e5f84651dcbd8cbea
birbirs\birbirsbirbirs <birbirsbirbirs@gmail.com>
2024-04-20 23:24:44 -0500

    tracer.createBaggageInScope cause duplicate traceId

--- src/main/java/co/pitam/bservice/config/RestInterceptor.java
+++ src/main/java/co/pitam/bservice/config/RestInterceptor.java
@@ -27,8 +27,8 @@ public class RestInterceptor extends OncePerRequestFilter {
 
         Map<String, String> allBaggage = tracer.getAllBaggage();
         List<String> fields = tracingProperties.getBaggage().getCorrelation().getFields();
-        fields.parallelStream().filter(p->!allBaggage.containsKey(p)).forEach(p->tracer.createBaggageInScope(p,null));
-
+//        fields.parallelStream().filter(p->!allBaggage.containsKey(p)).forEach(p->tracer.createBaggageInScope(p,null));
+//
         filterChain.doFilter(request, response);
     }
 }
