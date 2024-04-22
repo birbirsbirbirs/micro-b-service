9d48413bc2ce011b7db565b291aa37c523f3f746
birbirs\birbirsbirbirs <birbirsbirbirs@gmail.com>
2024-04-21 22:22:13 -0500

    yes yes yes

--- src/main/java/co/pitam/bservice/config/RestInterceptor.java
+++ src/main/java/co/pitam/bservice/config/RestInterceptor.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -13,6 +14,7 @@ import java.util.Enumeration;
 
 @Slf4j
 @Configuration
+@RequiredArgsConstructor
 public class RestInterceptor extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
