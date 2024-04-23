f1d742df82a0713ac762afd5b0c039456a50ef86
birbirs\birbirsbirbirs <birbirsbirbirs@gmail.com>
2024-04-23 14:49:51 -0500

    rabbit mq updated

---
+++ src/main/java/co/pitam/bservice/ptmSQS/HeroRabbitListener.java
@@ -0,0 +1,28 @@
+package co.pitam.bservice.ptmSQS;
+
+
+import co.pitam.bservice.model.Hero;
+import co.pitam.bservice.service.PtmAsynService;
+import io.micrometer.tracing.Tracer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Consumer;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class HeroRabbitListener {
+    private final PtmAsynService ptmAsynService;
+    private final Tracer tracer;
+
+    @Bean
+    public Consumer<Hero> ptmrabbit() {
+        return i -> {
+            log.info("{}", i);
+            ptmAsynService.runLog();
+        };
+    }
+}
